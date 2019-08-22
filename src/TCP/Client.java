3de614093bfa1756a36a27618a367da8569bc263
Joao Bitelo <joao.octavio.bitelo@gmail.com>
2019-08-22 19:10:25 -0300

    tcp pronto

--- src/TCP/Client.java
+++ src/TCP/Client.java
@@ -1,22 +1,74 @@
 package TCP;
 
-import java.io.ObjectInputStream;
-import java.net.Socket;
-import java.util.Date;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author sumatra
+ */
 
-import javax.swing.JOptionPane;
+import java.io.*;
+import java.net.*;
 
-public class Client {
-	public static void main(String[] args) {
+class Client {
+	private static Socket clientSocket;
+	private static DataOutputStream outToServer;
+	private static BufferedReader inFromServer;
+
+	public static void main(String argv[]) throws Exception {
+		createClientConnection();
+		String text = readFile();
+		sendMessage(text);
+
+	}
+
+	private static void sendMessage(String text) throws IOException {
+		/*
+		 * Envia para o servidor. Não esquecer do \n no final para permitir que o
+		 * servidor use readLine
+		 */
+		outToServer.writeBytes(text + '\n');
+
+		/* Lê mensagem de resposta do servidor */
+		String echo = inFromServer.readLine();
+
+		System.out.println("FROM SERVER: " + echo);
+
+		/* Encerra conexão */
+		clientSocket.close();
+
+	}
+
+	private static String readFile() throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader("texto.txt"));
 		try {
-			Socket cliente = new Socket("paulo", 12345);
-			ObjectInputStream entrada = new ObjectInputStream(cliente.getInputStream());
-			Date data_atual = (Date) entrada.readObject();
-			JOptionPane.showMessageDialog(null, "Data recebida do servidor:" + data_atual.toString());
-			entrada.close();
-			System.out.println("Conexão encerrada");
-		} catch (Exception e) {
-			System.out.println("Erro: " + e.getMessage());
+			StringBuilder sb = new StringBuilder();
+			String line = br.readLine();
+			while (line != null) {
+				sb.append(line);
+				line = br.readLine();
+			}
+			String everything = sb.toString();
+			System.out.println("arquivo lido");
+			return everything;
+		} finally {
+			br.close();
 		}
+
+	}
+
+	private static void createClientConnection() throws UnknownHostException, IOException {
+		/* Cria o socket cliente indicando o IP e porta de destino. */
+		clientSocket = new Socket("127.0.0.1", 6790);
+
+		/* Cria uma stream de saída para enviar dados para o servidor */
+		outToServer = new DataOutputStream(clientSocket.getOutputStream());
+
+		/* Cria uma stream de entrada para receber os dados do servidor */
+		inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
 	}
 }
--- src/TCP/Server.java
+++ src/TCP/Server.java
@@ -1,31 +1,127 @@
 package TCP;
 
-import java.io.ObjectOutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Date;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author sumatra
+ */
 
-public class Server {
-	  public static void main(String[] args) {
-	    try {
-	      // Instancia o ServerSocket ouvindo a porta 12345
-	      ServerSocket servidor = new ServerSocket(12345);
-	      System.out.println("Servidor ouvindo a porta 12345");
-	      while(true) {
-	        // o método accept() bloqueia a execução até que
-	        // o servidor receba um pedido de conexão
-	        Socket cliente = servidor.accept();
-	        System.out.println("Cliente conectado: " + cliente.getInetAddress().getHostAddress());
-	        ObjectOutputStream saida = new ObjectOutputStream(cliente.getOutputStream());
-	        saida.flush();
-	        saida.writeObject(new Date());
-	        saida.close();
-	        cliente.close();
-	      }  
-	    }   
-	    catch(Exception e) {
-	       System.out.println("Erro: " + e.getMessage());
-	    }
-	    finally {}  
-	  }     
+import java.io.*;
+import java.net.*;
+
+class Server {
+	private static Socket connectionSocket;
+	private static BufferedReader inFromClient;
+	private static DataOutputStream outToClient;
+
+	public static void main(String argv[]) throws Exception {
+
+		/* Cria socket do servidor */
+		ServerSocket welcomeSocket = new ServerSocket(6790);
+
+		while (true) {
+			createClientConnection(welcomeSocket);
+			String echo = getMessage();
+			boolean test = testMessage(echo);
+			answerClient(test);
+			createLocalFileandWriteContent(echo);
+
+		}
 	}
+
+	private static void createLocalFileandWriteContent(String echo) throws IOException {
+		File yourFile = new File("score.txt");
+		yourFile.createNewFile(); // if file already exists will do nothing
+		FileOutputStream oFile = new FileOutputStream(yourFile, false);
+
+		try (FileWriter writer = new FileWriter(yourFile); BufferedWriter bw = new BufferedWriter(writer)) {
+
+			bw.write(echo);
+
+		} catch (IOException e) {
+			System.err.format("IOException: %s%n", e);
+		}
+
+	}
+
+	private static void answerClient(boolean test) throws IOException {
+		String answer;
+		if (test == true) {
+			answer = "texto recebido corretamente";
+			System.out.println(answer);
+		} else {
+			answer = "texto NÃO recebido corretamente";
+			System.out.println(answer);
+		}
+
+		answer = answer + '\n';
+		/* Envia mensagem para o cliente */
+		outToClient.writeBytes(answer);
+
+		/* Encerra socket do cliente */
+		connectionSocket.close();
+
+	}
+
+	private static boolean testMessage(String echo) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader("texto.txt"));
+		try {
+			StringBuilder sb = new StringBuilder();
+			String line = br.readLine();
+			while (line != null) {
+				sb.append(line);
+				line = br.readLine();
+			}
+			String everything = sb.toString();
+			everything = everything + '\n';
+			if (everything.contentEquals(echo)) {
+				return true;
+			} else {
+				return false;
+			}
+
+		} finally {
+			br.close();
+		}
+
+	}
+
+	private static String getMessage() throws IOException {
+		/*
+		 * Aguarda o envio de uma mensagem do cliente. Esta mensagem deve ser terminada
+		 * em \n ou \r Neste exemplo espera-se que a mensagem seja textual (string).
+		 * Para ler dados não textuais tente a chamada read()
+		 */
+		String clientSentence = inFromClient.readLine();
+
+		/* Determina o IP e Porta de origem */
+		InetAddress IPAddress = connectionSocket.getInetAddress();
+		int port = connectionSocket.getPort();
+
+		/* Exibe, IP:port => msg */
+		System.out.println(IPAddress.getHostAddress() + ":" + port + " => " + clientSentence);
+
+		/* Adiciona o \n para que o cliente também possa ler usando readLine() */
+		return clientSentence + '\n';
+
+	}
+
+	private static void createClientConnection(ServerSocket welcomeSocket) throws IOException {
+		/*
+		 * Aguarda o recebimento de uma conexão. O servidor fica aguardando neste ponto
+		 * até que nova conexão seja aceita.
+		 */
+		connectionSocket = welcomeSocket.accept();
+
+		/* Cria uma stream de entrada para receber os dados do cliente */
+		inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+
+		/* Cria uma stream de saída para enviar dados para o cliente */
+		outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+
+	}
+}
